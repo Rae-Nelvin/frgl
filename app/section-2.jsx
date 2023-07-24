c8fc2fd401f6d4037d826f50cf7da24f75b786c0
Leonardo Wijaya <leonardo.wijaya003@binus.ac.id>
2023-07-24 13:05:14 +0700

    (feat) UI : Responsive for 1080p

--- app/section-2.jsx
+++ app/section-2.jsx
@@ -7,20 +7,20 @@ export default function Section2() {
             <div className='flex flex-row'>
                 <div className='flex flex-col w-1/2'>
                     <div className='mt-[-5%] relative'>
-                        <Image src={"icons/Section-2-Line.svg"} width={411} height={455} alt='Section 2 Line SVG' className='w-[60%] mt-[-30%]'></Image>
-                        <Image src={"icons/Section-2-Star.svg"} width={148} height={148} alt='Section 2 Star SVG' className='absolute bottom-[-32%] left-[25%] w-[20%]'></Image>
+                        <img src='icons/Section-2-Line.svg' alt='Section 2 Line SVG' className='w-[50%] mt-[-15%] 3xl:w-[60%] 3xl:mt-[-30%]'></img>
+                        <img src='icons/Section-2-Star.svg' alt='Section 2 Star SVG' className='absolute bottom-[-32%] left-[20%] 3xl:left-[25%] w-[20%]'></img>
                     </div>
                     <div className='flex flex-col font-helvetica text-[#0C1C32] mt-20 ml-40'>
-                        <h1 className='text-[160px] font-bold'>Enjoy Discount Up to 70%</h1>
-                        <h2 className='text-3xl text-[#6a7481] font-normal mt-12'>Exclusively for this month. Fashion fade, style is eternal. Style is something each of us alread has, all we need to do is find it, grab yours now</h2>
-                        <button className='bg-[#FB9910] rounded-lg font-light text-white py-10 w-[40%] mt-16 text-4xl tracking-[0.3em] uppercase'>Shop Now</button>
+                        <h1 className='text-[100px] 3xl:text-[160px] font-bold'>Enjoy Discount Up to 70%</h1>
+                        <h2 className='text-2xl 3xl:text-3xl text-[#6a7481] font-normal mt-12'>Exclusively for this month. Fashion fade, style is eternal. Style is something each of us alread has, all we need to do is find it, grab yours now</h2>
+                        <button className='bg-[#FB9910] rounded-lg font-light text-white py-6 3xl:py-10 w-[40%] 3xl:w-[40%] mt-16 text-2xl 3xl:text-4xl tracking-[0.3em] uppercase'>Shop Now</button>
                     </div>
                 </div>
                 <div className='flex flex-col w-1/2'>
                     <div className='w-full relative ml-80'>
-                        <Image src={"/images/section-2-1.png"} width={538} height={876} alt='Section 2 Picture 1' className='w-[60%]'></Image>
-                        <Image src={"/images/section-2-2.png"} width={363} height={642} alt='Section 2 Picture 2' className='absolute top-[60%] left-[30%] w-[40%]'></Image>
-                        <Image src={"/images/section-2-3.png"} width={342} height={316} alt='Section 2 Picture 3' className='absolute top-[180%] left-[10%] w-[35%]'></Image>
+                        <img src='/images/section-2-1.png' alt='Section 2 Picture 1' className='w-[50%] 3xl:w-[60%]'></img>
+                        <img src='/images/section-2-2.png' alt='Section 2 Picture 2' className='absolute top-[60%] left-[25%] 3xl:left-[30%] w-[30%] 3xl:w-[40%]'></img>
+                        <img src='/images/section-2-3.png' alt='Section 2 Picture 3' className='absolute top-[180%] left-[5%] 3xl:left-[10%] w-[30%] 3xl:w-[35%]'></img>
                     </div>
                 </div>
             </div>
--- app/section-3.jsx
+++ app/section-3.jsx
@@ -10,7 +10,7 @@ export default function Section3() {
                     <h1 className="text-[96px] font-bold uppercase">New Items</h1>
                     <div className='flex flex-row space-x-2'>
                         <h2 className='text-3xl font-normal leading-tight'>Look at the collection</h2>
-                        <Image src={"icons/Arrow-Right.svg"} width={24} height={24} alt='Arrow Right SVG' className='w-10'></Image>
+                        <img src='icons/Arrow-Right.svg'alt='Arrow Right SVG' className='w-10'></img>
                     </div>
                 </div>
                 <div className='flex flex-row space-x-40 z-10 mt-60'>
@@ -18,24 +18,24 @@ export default function Section3() {
                         <div className='bg-[#FB9910] rounded-full text-white py-20 px-16 absolute right-[-15%] top-[-10%]'>
                             <h1 className='font-bold text-3xl'>View</h1>
                         </div>
-                        <Image src={"/images/section-3-1.png"} width={336} height={578} alt='Section 3 Image 1' className='w-[580px] mb-3'></Image>
-                        <h2 className='font-bold text-3xl mb-1'>Sweater Putih Keren</h2>
-                        <h3 className='font-normal text-2xl'>$75</h3>
+                        <img src='/images/section-3-1.png' alt='Section 3 Image 1' className='w-[400px] 3xl:w-[580px] mb-3'></img>
+                        <h2 className='font-bold text-2xl 3xl:text-3xl mb-1'>Sweater Putih Keren</h2>
+                        <h3 className='font-normal text-xl 3xl:text-2xl'>$75</h3>
                     </div>
                     <div className='flex flex-col z-10'>
-                        <Image src={"/images/section-3-2.png"} width={336} height={578} alt='Section 3 Image 2' className='w-[580px] mb-3'></Image>
-                        <h2 className='font-bold text-3xl mb-1'>Jacket Luntur</h2>
-                        <h3 className='font-normal text-2xl'>$120</h3>
+                        <img src='/images/section-3-2.png' alt='Section 3 Image 2' className='w-[400px] 3xl:w-[580px] mb-3'></img>
+                        <h2 className='font-bold text-2xl 3xl:text-3xl mb-1'>Jacket Luntur</h2>
+                        <h3 className='font-normal text-xl 3xl:text-2xl'>$120</h3>
                     </div>
                     <div className='flex flex-col z-10'>
-                        <Image src={"/images/section-3-3.png"} width={336} height={578} alt='Section 3 Image 3' className='w-[580px] mb-3'></Image>
-                        <h2 className='font-bold text-3xl mb-1'>Kocomoto Kuda</h2>
-                        <h3 className='font-normal text-2xl'>$2.000</h3>
+                        <img src='/images/section-3-3.png' alt='Section 3 Image 3' className='w-[400px] 3xl:w-[580px] mb-3'></img>
+                        <h2 className='font-bold text-2xl 3xl:text-3xl mb-1'>Kocomoto Kuda</h2>
+                        <h3 className='font-normal text-xl 3xl:text-2xl'>$2.000</h3>
                     </div>
                     <div className='flex flex-col z-10'>
-                        <Image src={"/images/section-3-4.png"} width={336} height={578} alt='Section 3 Image 4' className='w-[580px] mb-3'></Image>
-                        <h2 className='font-bold text-3xl mb-1'>Keram Pants</h2>
-                        <h3 className='font-normal text-2xl'>$127</h3>
+                        <img src='/images/section-3-4.png' alt='Section 3 Image 4' className='w-[400px] 3xl:w-[580px] mb-3'></img>
+                        <h2 className='font-bold text-2xl 3xl:text-3xl mb-1'>Keram Pants</h2>
+                        <h3 className='font-normal text-xl 3xl:text-2xl'>$127</h3>
                     </div>
                 </div>
             </div>
